0331ac7a526c8b9745f73629b7518afb93c34ff7
gopal-211 <learncode973@gmail.com>
2024-03-07 00:06:08 +0530

    initalized project with vite tailwind shadcn

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import { useWallet,useDisconnect,ConnectWallet } from "@thirdweb-dev/react"
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <button></button>
+    </h1>
+  )
+}
